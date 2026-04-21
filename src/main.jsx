833d795873ea752edd6050bba4bfd434036e2f8f
Daria Timanova <dg30092002@gmail.com>
2026-04-22 02:30:29 +0300

    feat: setup routing and Redux store with Ant Design locale

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import App from './App';
+import { store } from './store/store';
+import 'antd/dist/reset.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ConfigProvider locale={ruRU} theme={{
+          token: {
+            colorPrimary: '#1677ff',
+            borderRadius: 8,
+          },
+        }}>
+          <App />
+        </ConfigProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
